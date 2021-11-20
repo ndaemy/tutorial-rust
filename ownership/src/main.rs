913d9ab8ea0ff9bfce995d947038ec401953eead
ndaemy <yuyaebean@gmail.com>
2021-11-20 23:49:52 +0900

    4.3 The Slice Type

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
-    let mut s = String::from("hello");
+    let mut s = String::from("hello world");
 
-    change(&mut s);
+    let word = first_word(&s);
+
+    println!("{}", first_word(&mut s));
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }

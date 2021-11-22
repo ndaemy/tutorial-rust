d93800d1f685cda08ea1e3fef5de5e7fb5e97f94
ndaemy <yuyaebean@gmail.com>
2021-11-22 15:58:31 +0900

    5.2 An Example Program Using Structs

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -1,41 +1,15 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
-
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
-
-struct AlwaysEqual;
 
 fn main() {
-    let mut user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
     };
 
-    user1.email = String::from("anotheremail@example.com");
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
-
-    let subject = AlwaysEqual;
-}
-
-fn build_user(email: String, username: String) -> ser {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+    dbg!(&rect1);
 }

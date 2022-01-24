b77444b706ec5c24ed1ef1238b3a2dc31fce008f
ndaemy <yuyaebean@gmail.com>
2022-01-24 15:09:01 +0900

    6.2 The match Control Flow Operator

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,17 +1,11 @@
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self) {
-        // method body would be defined here
+fn main() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
     }
 }
 
-fn main() {
-    let m = Message::Write(String::from("hello"));
-    m.call();
-}
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}

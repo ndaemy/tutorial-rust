234aa1d8d8116d26b9871f0a20f048e569a8e370
ndaemy <yuyaebean@gmail.com>
2022-02-02 04:15:09 +0900

    7.4 Bringing Paths Into Scope with the use Keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -27,13 +27,8 @@ mod back_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    // Order a breakfast in the summer with Rye toast
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    // Change our mind about what bread we'd like
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+pub use crate::front_of_house::hosting;
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist()
 }

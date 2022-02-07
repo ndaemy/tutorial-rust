7077bf23a17fbf15f1636f9fb4e2237048c4ad62
ndaemy <yuyaebean@gmail.com>
2022-02-07 11:43:53 +0900

    9.2 Recoverable Errors with Result

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,56 @@
-fn main() {
-    let v = vec![1, 2, 3];
+use std::fs::File;
+use std::{fs, io};
+use std::io::Read;
 
-    v[99];
+fn main() {
+    // let f = File::open("hello.txt");
+    //
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
+
+    // let f = File::open("hello.txt").unwrap();
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    // let f = File::open("hello.txt");
+    //
+    // let mut f = match f {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e),
+    // };
+    //
+    // let mut s = String::new();
+    //
+    // match f.read_to_string(&mut s) {
+    //     Ok(_) => Ok(s),
+    //     Err(e) => Err(e),
+    // }
+
+    // ---
+
+    // let mut f = File::open("hello.txt");
+    // let mut s = String::new();
+    // f.read_to_string(&mut s)?;
+    // Ok(s)
+
+    // ---
+
+    // let mut s = String::new();
+    //
+    // File::open("hello.txt")?.read_to_string(&mut s)?;
+    //
+    // Ok(s)
+
+    fs::read_to_string("hello.txt")
 }

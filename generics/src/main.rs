2c6cb605fecf9c6ec5a29b1e70180ffdc55020c8
ndaemy <yuyaebean@gmail.com>
2022-02-23 23:58:50 +0900

    10.1 Generic Data Types

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -10,14 +10,29 @@ fn largest(list: &[i32]) -> i32 {
     largest
 }
 
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest(&number_list);
     println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
 }
